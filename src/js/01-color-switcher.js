59b6d296736f96a3b0eb6b2e8b48fbe7662e0010
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-22 16:16:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,25 @@
-const btnStart = document.querySelector('data-start');
-const btnStop = document.querySelector('data-stop');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+btnStart.addEventListener('click', onClickBtnStart);
+btnStop.addEventListener('click', onClickBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onClickBtnStart(event) {
+    btnStart.setAttribute('disabled', '');
+    const intervalId = setInterval(() => {
+        if (!btnStart.hasAttribute('disabled')) {
+            return;
+            clearInterval(intervalId);
+        }
+     bodyEl.style.backgroundColor = getRandomHexColor();    
+}, 1000)
+}
+ 
+function onClickBtnStop(event) {
+    btnStart.removeAttribute('disabled');
+}
