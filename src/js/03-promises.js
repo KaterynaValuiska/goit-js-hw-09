b732adc06b38f28bfdb5cdbb34a635e9e84be749
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-29 16:54:38 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', createPromise);
+
+const delay = 2000;
+const STEP = 1000;
+const amount = 6;
+
+let position = 0;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
+    const intervalPromise = setInterval(() => {
+      position += 1;
+      delay += STEP;
+      if (position === amount) {
+        clearInterval(intervalPromise);
+      }
+      
+      console.log(position, delay);
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+ 
+  });
+  return promise;
+};
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
