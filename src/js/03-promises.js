075b8e5794d4788104048bd28590f23911b0f094
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-05-29 20:40:58 +0200

    fix 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,15 @@
-const delayEl = document.querySelector('input[name="delay"]');
-const stepEl = document.querySelector('input[name="step"]');
-const amountEl = document.querySelector('input[name="amount"]');
+import Notiflix from 'notiflix';
 const formEl = document.querySelector('.form');
 
-formEl.addEventListener('submit', createPromise);
+formEl.addEventListener('submit', onSubmitForm);
 
-const delay = 2000;
-const STEP = 500;
-const amount = 6;
-
-let position = 0;
 
 function createPromise(position, delay) {
   
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     
-    const intervalPromise = setInterval(() => {
-      position += 1;
-      delay += STEP;
-      if (position === amount) {
-        clearInterval(intervalPromise);
-      }
-      
-      console.log(position, delay);
+    setTimeout(() => {
 
       if (shouldResolve) {
         resolve({ position, delay });
@@ -33,13 +19,25 @@ function createPromise(position, delay) {
     }, delay);
  
   });
-  return promise;
 };
 
-createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.target.elements;
+
+  for (let i = 0; i < amount.value; i++) {
+    position = i + 1;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+    del = Number(delay.value) + i * Number(step.value);
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${del}ms`);
   })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      .catch(({ position, delay }) => {
+    del = Number(delay.value) + i * Number(step.value);
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${del}ms`);
   });
+    
+  }
+}
+
