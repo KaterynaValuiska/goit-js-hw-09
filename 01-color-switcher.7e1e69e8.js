5f67db32d13cf853d17306115fd158ea4071a7b9
KaterynaValuiska <122235679+KaterynaValuiska@users.noreply.github.com>
2023-05-30 08:50:35 +0000

    Deploying to gh-pages from @ KaterynaValuiska/goit-js-hw-09@61fb88e4b7ead7c455505073d36b24bf933ce3ff 🚀

--- 01-color-switcher.7e1e69e8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){n=setInterval((function(){t.setAttribute("disabled",""),r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(e){t.removeAttribute("disabled"),clearInterval(n)}));var n=null}();
-//# sourceMappingURL=01-color-switcher.7e1e69e8.js.map
